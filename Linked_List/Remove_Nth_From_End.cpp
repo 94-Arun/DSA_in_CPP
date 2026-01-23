a2007054c9c92e59c6b152cbef898537321758a8
Arun Kumar <sonukalkal594@gmail.com>
2026-01-24 00:38:47 +0530

    remove Nth at the end of the linked list

---
+++ Linked_List/Remove_Nth_From_End.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+
+    // ~Node(){
+    //     cout<<"~Node "<<data<<endl;
+    //     if(next != NULL){
+    //         delete next;
+    //         next = NULL;
+    //     }
+    // }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = NULL;
+        tail = NULL;
+    } 
+
+    // ~List(){
+    //     cout<<"~List"<<endl;
+    //     if(head != NULL){
+    //         delete head;
+    //         head = NULL;
+    //     }
+    // }
+    
+    // operations on linked list
+    
+    void push_front(int data){
+        Node* newnode = new Node(data);
+
+        if (head == NULL){
+            head = tail = newnode;
+        }
+        else{
+            newnode->next = head;
+            head = newnode;
+        }
+
+    }
+
+    void display(){
+        Node* temp = head;
+        if(head == NULL) cout<<"Empty List:"<<endl;
+        else{
+            while(temp!=NULL){
+                cout<<temp->data<<"->";
+                temp = temp->next;
+            } 
+            cout<<"NULL";
+            cout<<endl;
+            
+        }
+    }
+
+    void push_back(int val){
+        Node* newnode = new Node(val);
+        if (head == NULL){
+            head = tail = newnode;
+        }else{
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+    int getSize(){
+        Node* temp = head;
+        int count = 0;
+        while(temp != NULL){
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+
+    void removeNth(int n){
+        int size = getSize();
+
+        // For Invalid Position
+        if(n<0 || n>size){
+            cout<<"Invalid Position";
+            return ;
+        }
+
+        // if todelete is head
+        if(n == size){
+            Node* temp = head;
+            head = temp->next;
+            delete temp;
+        }
+
+
+        Node* prev = head;
+
+        for(int i = 1;i<(size-n);i++){
+            prev = prev->next;
+        }
+        Node* todelete = prev->next;
+        prev->next = todelete->next;
+
+        delete todelete;
+    }  
+};
+
+int main(){ 
+
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+
+    ll.display();
+    // cout<<ll.getSize();
+    ll.removeNth(3);
+    ll.display();
+
+
+
+}
+ 
---
+++ Linked_List/Reverse_LL.cpp
@@ -0,0 +1,158 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+
+    // ~Node(){
+    //     cout<<"~Node "<<data<<endl;
+    //     if(next != NULL){
+    //         delete next;
+    //         next = NULL;
+    //     }
+    // }
+};
+
+class List{
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = NULL;
+        tail = NULL;
+    } 
+
+    // ~List(){
+    //     cout<<"~List"<<endl;
+    //     if(head != NULL){
+    //         delete head;
+    //         head = NULL;
+    //     }
+    // }
+    
+    // operations on linked list
+    
+    void push_front(int data){
+        Node* newnode = new Node(data);
+
+        if (head == NULL){
+            head = tail = newnode;
+        }
+        else{
+            newnode->next = head;
+            head = newnode;
+        }
+
+    }
+
+    void display(){
+        Node* temp = head;
+        if(head == NULL) cout<<"Empty List:"<<endl;
+        else{
+            while(temp!=NULL){
+                cout<<temp->data<<"->";
+                temp = temp->next;
+            } 
+            cout<<"NULL";
+            cout<<endl;
+            
+        }
+    }
+
+    void push_back(int val){
+        Node* newnode = new Node(val);
+        if (head == NULL){
+            head = tail = newnode;
+        }else{
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+    void insert(int val,int pos){
+        Node* newnode = new Node(val);
+        Node* temp = head;
+
+        for(int i = 0;i<pos-1;i++){
+            if(temp==NULL){
+                cout<<"Invalid Position:";
+                return;
+
+            }
+            temp = temp->next;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;   
+    }
+
+    // void pop_front(){
+    //     Node* temp = head;
+    //     if(temp == NULL) cout<<"List Dosnt Exist";
+    //     head = head->next;
+    //     temp->next = NULL;
+    //     delete temp;
+    // }
+
+    // void pop_back(){
+    //     Node* temp = head;
+    //     while(temp->next != tail){
+    //         temp = temp->next;
+    //     }
+    //     temp->next = NULL;
+    //     delete tail;
+    //     tail = temp;
+    // }
+
+
+    void reverse(){
+        Node* prev = NULL;
+        Node* curr = head;
+
+        while(curr!=NULL){
+            Node* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        tail = head;
+        head = prev;
+
+    }
+};
+
+
+
+int main(){ 
+
+    List ll;
+
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    // ll.display();
+    // cout<<endl;
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+    // ll.display();
+    // cout<<endl;
+    ll.insert(10,3);
+    ll.insert(7,6);
+    // ll.display();
+    // ll.pop_front();
+    ll.display();
+    ll.reverse();
+    ll.display();
+
+
+}
+ 
