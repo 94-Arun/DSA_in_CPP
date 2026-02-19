c6a3f85697453f59d6d27606ac91bb9c3193e623
Arun Kumar <sonukalkal594@gmail.com>
2026-02-19 23:16:11 +0530

    Duplicate Paranthesis

---
+++ Stack/Duplicate_Paranthesis.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+bool duplicateParanthesis(string str){
+    stack<char> s;
+    
+    for(int i = 0; i<str.size();i++){
+        char ch = str[i];
+
+        if(ch != ')') s.push(ch);
+        else{
+            if(s.top() == '(') return true;
+            while(s.top() != '(') s.pop();
+            s.pop();
+        }
+    }
+    return false;
+}
+
+int main(){
+    string str1 = "((a+b))";
+    string str2 = "((a+b)+(a+b))";
+
+    cout<<duplicateParanthesis(str1)<<endl;
+    cout<<duplicateParanthesis(str2)<<endl;
+
+    return 0;
+}
