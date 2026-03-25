b6a72e965d30c603d165b4164a81462c420dfb6b
Arun Kumar <sonukalkal594@gmail.com>
2026-03-25 21:41:55 +0530

    QUEUE

---
+++ Queue/01.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:    
+    int data;
+    Node *next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+
+};
+// queue class
+class Queue{
+    Node *front;
+    Node *rear;
+
+public:
+    Queue(){
+        front = NULL;
+        rear = NULL;
+    }
+
+    void enqueue(int val){
+        Node *newnode = new Node(val);
+        if(front == NULL){
+            front = rear = newnode;
+            return;
+        }
+        rear->next = newnode;
+        rear = newnode;
+    }
+    int dequeue(){
+        if(front == NULL){
+            cout<<"QUEUE IS Empty";
+            return -1;
+        }  
+        Node *temp = front;
+        front = front->next;
+        temp->next = NULL;
+        int data = temp->data;
+        delete temp;
+        if(front == NULL){
+            rear = NULL;
+        }
+        return data;
+    }
+    void display(){
+        Node *temp = front;
+        while(temp != NULL){
+            cout<<temp->data<<" ";
+            temp = temp->next;    
+        }
+        cout<<endl<<"NULL"<<endl;
+    }
+};
+
+int main(){
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.display();
+    cout<<"Deleted Element:"<<q.dequeue()<<endl;
+    q.display();
+
+
+
+    return 0;
+}
---
+++ Queue/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+"NULL";
+    }
+};
+
+int main(){
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
