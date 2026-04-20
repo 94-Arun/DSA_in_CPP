faf4172924e920fe22476dfed7e850c3ca192149
94-Arun <sonukalkal594@gmail.com>
2026-04-20 23:50:17 +0530

    sddf

--- Queue/CircularQueue.cpp
+++ Queue/CircularQueue.cpp
@@ -1,2 +1,88 @@
 #include<iostream>
 using namespace std;
+
+class Queue{
+    int *arr;
+
+    int capacity;
+    int currsize;
+
+    int f,r;
+public:
+    Queue(int n){
+        this->capacity = n;
+        arr = new int[n];
+        currsize = 0;
+        f = 0;
+        r = -1;
+    }
+
+    void push(int val){
+        if(currsize == capacity) {
+            cout<<"Queue is full\n";
+            return;
+        } 
+        r = (r+1)%capacity;
+        arr[r] = val;
+        currsize++;
+
+    }
+
+    void pop(){
+        if(isEmpty()){
+            cout<<"Queue is empty:\n";
+            return;
+        }
+        f = (f+1)%capacity;
+        currsize--; 
+    }
+
+    int front(){
+        if(isEmpty()){
+            cout<<"Queue is empty:\n";
+            return -1;
+        }
+        return arr[f];
+    }
+
+    bool isEmpty(){
+        return currsize == 0;
+    }
+
+    void printQueue(){
+        if(isEmpty()){
+            cout<<"Queue is empty:\n";
+            return;
+        }
+        int i = 0;
+        while(i != r+1){
+            cout<<arr[i]<<" ";
+            i++;
+        }
+        cout<<endl;
+
+    }
+
+
+};
+
+int main(){
+    Queue q(5);
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    // q.push(7);
+ 
+    q.printQueue();
+
+    q.pop();
+    q.printQueue();
+
+
+
+    return 0;
+}
