5acf567da5d5c508af5509850fadcc558e351fbf
94-Arun <sonukalkal594@gmail.com>
2026-04-26 00:57:52 +0530

    Queue using 2 stacks

--- Queue/QueueByTwoStacks.cpp
+++ Queue/QueueByTwoStacks.cpp
@@ -3,41 +3,95 @@
 using namespace std;
 
 
+// Method : Eager Transfer
+//push() -> O(n)
+//pop() -> O(1)
+// class Queue{
+//     stack<int> s1;
+//     stack<int> s2;
+// public:
+//     // push operation O(n) time complexity
+//     void push(int a){
+//         // s1 -> s2
+//         while(!s1.empty()){
+//             s2.push(s1.top());
+//             s1.pop();
+//         }
+//         s1.push(a);
+
+//         // s1 -> s1
+//         while(!s2.empty()){
+//             s1.push(s2.top());
+//             s2.pop();
+//         }
+//     }   
+
+//     void pop(){
+//         if(isempty()){
+//             cout << "Queue is empty\n";
+//             return;
+//         }
+//         s1.pop();
+//     }
+
+//     int front(){
+//         if(isempty()){
+//             cout << "Queue is empty\n";
+//             return -1;
+//         }
+//         return s1.top();
+//     }
+
+//     bool isempty(){
+//         return s1.empty();
+//     }
+
+// };
+
+// Method: Lazy Transfer
+// pop() -> Amortized(1);
+// push()-> O(1);
 class Queue{
     stack<int> s1;
     stack<int> s2;
 public:
-    // push operation O(n) time complexity
-    void push(int a){
-        // s1 -> s2
+     
+void push(int a){
+    s1.push(a);
+}
+
+void pop(){
+    if(isempty()){
+        cout << "Queue is empty\n";
+        return;
+    }
+    if(s2.empty()){
         while(!s1.empty()){
             s2.push(s1.top());
             s1.pop();
         }
-        s1.push(a);
-
-        // s1 -> s1
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
+    }
+    s2.pop();
+}
+int front(){
+    if(isempty()){
+        cout << "Queue is empty\n";
+        return -1;
+    }
+    if(s2.empty()){
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
         }
-    }   
-
-    // O(1)
-    void pop(){
-        s1.pop();
     }
-
-    // O(1)
-    int front(){
-         return s1.top();
-    }
-
-    bool isempty(){
-        return s1.empty();
-    }
+    return s2.top();
+}
+bool isempty(){
+    return s1.empty() && s2.empty();
+}
 
 };
+
 
 
 
@@ -47,6 +101,7 @@ int main(){
     q.push(1);
     q.push(2);
     q.push(3);
+    // q.pop();
 
     while(!q.isempty()){
         cout<<q.front()<<endl;
