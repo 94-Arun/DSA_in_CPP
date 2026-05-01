fc98327ef6dd3f29d676d4f97c4b5f94246a857f
94-Arun <sonukalkal594@gmail.com>
2026-05-01 22:46:50 +0530

    implementation using Deque

---
+++ Queue/StackUsingDeque.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+class Stack{
+    deque<int> deq;
+public:
+
+    void push(int data){
+        deq.push_front(data);
+    }
+    void pop(){
+        deq.pop_front();
+    }
+    int top(){
+        return deq.front();
+    }
+    bool empty(){
+        return deq.empty();
+    }
+
+};
+
+
+int main(){
+
+    Stack s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    cout<<s.top()<<endl;
+    cout<<endl;
+
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+
+    return 0;
+}
